3b6bc0f9ed75568bb747aa8e5d2605e84dd6be73
Zacharia Njiraini <84382705+maaszaxxs@users.noreply.github.com>
2021-11-25 16:38:57 +0300

    Add files via upload
    aws-restart

---
+++ hello-world.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+print(len("malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"))
+print(len("giveqcctsicslyqlenycn"))
